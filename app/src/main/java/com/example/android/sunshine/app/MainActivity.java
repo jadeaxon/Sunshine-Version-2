3dca32393f25da4105a786108960ec83f9a23bee
Jeff Anderson <jeff.anderson@digecor.com>
2015-10-29 10:28:45 -0600

    Show String[] in ListView via ArrayAdapter<String>

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -54,10 +58,43 @@ public class MainActivity extends ActionBarActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
-}
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            // Forecasts to show in the main activity.
+            ArrayList<String> forecasts = new ArrayList<String>();
+            forecasts.add("Today -- Sunny");
+            forecasts.add("Monday -- Rainy");
+            forecasts.add("Tuesday -- Snow");
+            forecasts.add("Wednesday -- Hurricane");
+
+            String[] forecasts2 = {
+                    "Today -- Sunny -- 76/50",
+                    "Monday -- Rainy -- 55/40",
+                    "Tuesday -- Snow -- 26/17",
+                    "Wednesday -- Hurricane -- 60/50",
+                    "Thursday -- Eclipse -- 66/13",
+                    "Friday -- Full Moon -- 59/49",
+                    "Saturday -- Hot -- 110/90",
+                    "Sunday -- Windy -- 80/70",
+                    "Halloween -- Sweet -- 76/65"
+            };
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    this.getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textview,
+                    forecasts2
+            );
+
+            // This is the fragment itself.
+            View fragment = inflater.inflate(R.layout.fragment_main, container, false);
+
+            // We get the activity this fragment is a part of.
+            // Then we look up a widget in the activity by id.
+            // Then we bind a data set to that view widget via an adapter.
+            ListView v = (ListView) fragment.findViewById(R.id.listview_forecast);
+            v.setAdapter(adapter);
+
+            return fragment;
+        } // onCreateView(...)
+    } // class PlaceholderFragment
+} // class MainActivity
